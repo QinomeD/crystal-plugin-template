e3ff4060bc6bd0aca9334ff3f104085017a1aaa5
QinomeD <cpueater@interia.pl>
2024-02-09 08:57:19 +0100

    eventReceived, suppressed unused Main class

--- src/main/java/qinomed/crystalplugin/Main.java
+++ src/main/java/qinomed/crystalplugin/Main.java
@@ -1,12 +1,21 @@
 package qinomed.crystalplugin;
 
 import ovh.crystallauncher.crystal.plugin.AbstractPlugin;
+import ovh.crystallauncher.crystal.plugin.EPluginEventType;
 import ovh.crystallauncher.crystal.plugin.PluginManager;
 
+@SuppressWarnings("unused")
 public class Main extends AbstractPlugin {
     @Override
     public void onPluginLoad(PluginManager pluginManager) {
         // Do something on load
+    }
+
+    @Override
+    public void eventReceived(EPluginEventType eventType, Object ctx) {
+        // Listen for launcher events
+        // Check for a specific event type
+        // Cast ctx argument accordingly (if the context is present)
     }
 
     @Override
